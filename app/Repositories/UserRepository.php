8f42c1768021dacc2596d59a0a440ca611fafda4
tejaswi <tej.tejaswi115@gmail.com>
2024-11-29 18:01:05 +0530

    uncommented otp

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -281,8 +281,8 @@ class UserRepository implements UserRepositoryInterface
                 'template' => 'otp',
                 'to' => $existingRecord['user_details']['signup_data']['email']
             ];
-            // $otpURL='https://360marketingservice.com/api/v2/SendSMS?SenderId=VIAUBU&Is_Unicode=false&Is_Flash=false&Message='.$otpNum.'%20is%20your%20one-time%20password%20for%20your%20Kubera%20Account%20powered%20by%20%22VIINDHYA%20AU%20BULLION%20LLP%22.This%20OTP%20is%20valid%20only%20for%205%20minutes.&MobileNumbers='.$existingRecord['user_details']['signup_data']['phoneNumber'].'&ApiKey=bdWWoqrc54f1Q5mvoD21eogUirIZHU%2Bl%2BzoPL2NVEd8%3D&ClientId=304b754c-ca9b-4028-b59f-1d8a08bffb4f';
-            // $this->sendSMS($otpURL) ;
+            $otpURL='https://360marketingservice.com/api/v2/SendSMS?SenderId=VIAUBU&Is_Unicode=false&Is_Flash=false&Message='.$otpNum.'%20is%20your%20one-time%20password%20for%20your%20Kubera%20Account%20powered%20by%20%22VIINDHYA%20AU%20BULLION%20LLP%22.This%20OTP%20is%20valid%20only%20for%205%20minutes.&MobileNumbers='.$existingRecord['user_details']['signup_data']['phoneNumber'].'&ApiKey=bdWWoqrc54f1Q5mvoD21eogUirIZHU%2Bl%2BzoPL2NVEd8%3D&ClientId=304b754c-ca9b-4028-b59f-1d8a08bffb4f';
+            $this->sendSMS($otpURL) ;
             $this->sendEmail($data);
             return ['status'=>true, 'data'=> $existingRecord['user_details']['signup_data']['email']];
         }
